5d4df0bf8e968adac70e8d2703c6f5131169c3f0
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-06-16 18:09:43 +0900

    feat: Add react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 
+import { Link, Route, Routes } from 'react-router-dom';
+
 import Header from './Header';
 import MainList from './MainList';
 
@@ -10,6 +12,13 @@ function App () {
       <Header />
       <div className='main-banner'></div>
       <MainList />
+      <Link to="/">Home</Link>
+      <Link to="/detail">Detail</Link>
+      <Routes>
+      <Route path='/' element={<div>메인</div>}/>
+        <Route path='/detail' element={<div>상세페이지</div>}/>
+        <Route path='/myPage' element={<div>마이페이지</div>}/>
+      </Routes>
   </div> 
   );
 }
